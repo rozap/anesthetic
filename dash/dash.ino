4e9f6c246cdc0fe8381d220d2cbe59ef7a1a5b1d
rozap <chrisd1891@gmail.com>
2020-04-12 13:27:03 -0700

    wtf??

--- dash/dash.ino
+++ dash/dash.ino
@@ -6,21 +6,35 @@
 #define OIL_PRESSURE_CLK 26
 #define OIL_PRESSURE_DIO 27
 #define OIL_PRESSURE_WINDOW 5
+#define OIL_VIN_PIN 0
 
-#define TEST_DELAY   10
+#define TEST_DELAY   100
 
-const uint8_t SEG_HI[] = {
-  SEG_F | SEG_E | SEG_G | SEG_C, // h
-  SEG_C,   // i
-  0x00, // blank
-  0x00 // blank
+// https://www.makerguides.com/wp-content/uploads/2019/08/7-segment-display-annotated.jpg
+
+// anes
+// thet
+// ic
+const uint8_t SEG_SHIT[] = {
+  SEG_A | SEG_F | SEG_G | SEG_C | SEG_D,
+  SEG_F | SEG_E | SEG_B | SEG_C | SEG_G,
+  SEG_B | SEG_C,
+  SEG_F | SEG_E | SEG_A
   };
+const uint8_t SEG_FAIL[] = {
+  SEG_A | SEG_F | SEG_E | SEG_G,
+  SEG_A | SEG_F | SEG_B | SEG_E | SEG_C | SEG_G,
+  SEG_B | SEG_C,
+  SEG_F | SEG_E | SEG_D
+  };
+
+
 
 TM1637Display oilPressure(OIL_PRESSURE_CLK, OIL_PRESSURE_DIO);
 
 void setup() {
   oilPressure.setBrightness(0x0f);
-  oilPressure.setSegments(SEG_HI);
+  oilPressure.setSegments(SEG_FAIL);
 
   delay(5000);
 
@@ -34,27 +48,10 @@ void loop() {
   delay(TEST_DELAY);
 }
 
-// from https://www.autometer.com/sensor_specs
-// PSI   Ohms
-// 0     250
-// 25    158
-// 50    111
-// 75    75
-// 100   43
 CircularBuffer<float,OIL_PRESSURE_WINDOW> oilPSIWindow;
 
-int ohms = 43;
-int readOilSensorResistance() {
-  // replace this with the actual reading from the analog pin
-  ohms += 1;
-  if (ohms > 250) {
-    ohms = 43;
-  }
-  return ohms;
-}
-
 float readOilPSI() {
-  float psi = -0.4760 * readOilSensorResistance() + 110.6;
+  float psi = ((float) analogRead(OIL_VIN_PIN) / 1024) * 200;
   oilPSIWindow.push(psi);
 
   float total = 0;
