022a6b4dc837c8610bec0e438eacdc948eac3202
rozap <chrisd1891@gmail.com>
2025-02-09 19:58:00 -0800

    what happened to my files :(

---
+++ can-devices/gps/gps/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+
+  Serial.println("------- CAN Read ----------");
+  Serial.println("ID  DLC   DATA");
+}
+
+void loop()
+{
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
+  {
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" ");
+    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+
+    for (int i = 0; i < canMsg.can_dlc; i++)
+    { // print the data
+      Serial.print(canMsg.data[i], HEX);
+      Serial.print(" ");
+    }
+
+    Serial.println();
+  }
+}
+
+// put function definitions here:
+int myFunction(int x, int y)
+{
+  return x + y;
+}
---
+++ can-devices/uart-dump/uart-dump/src/main.cpp
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <mcp2515.h>
+
+struct can_frame canMsg;
+MCP2515 mcp2515(PA4);
+
+void setup()
+{
+  Serial1.begin(9600);
+
+  SPI.setMOSI(PA7);
+  SPI.setMISO(PA6);
+  SPI.setSCLK(PA5);
+  SPI.begin();
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+
+  Serial1.println("------- CAN Read ----------");
+  Serial1.println("ID  DLC   DATA");
+}
+
+void loop()
+{
+  MCP2515::ERROR res = mcp2515.readMessage(&canMsg);
+  if (res == MCP2515::ERROR_OK)
+  {
+    Serial1.print(canMsg.can_id, HEX); // print ID
+    Serial1.print(" ");
+    Serial1.print(canMsg.can_dlc, HEX); // print DLC
+    Serial1.print(" ");
+
+    for (int i = 0; i < canMsg.can_dlc; i++)
+    { // print the data
+      Serial1.print(canMsg.data[i], HEX);
+      Serial1.print(" ");
+    }
+
+    Serial1.println();
+  }
+  else
+  {
+    Serial1.print("No message: ");
+    Serial1.print(res);
+    Serial1.println();
+    delay(500);
+  }
+}
